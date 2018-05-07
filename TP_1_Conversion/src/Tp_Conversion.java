c4660475f44ff3d5d0b7209a660a0414b49d9a5a
Jean Penan <jeanpenangoumou1@gmail.com>
2018-05-08 01:21:48 +0200

    Utilisation de la boucle while pour le développement de l'application, et mise en oeuvre de la méthode arrondi pour arrondir les résultats de conversions.

--- TP_1_Conversion/src/Tp_Conversion.java
+++ TP_1_Conversion/src/Tp_Conversion.java
@@ -8,7 +8,40 @@ public class Tp_Conversion {
 		double arrondC, arrondF;
 		Scanner sc = new Scanner(System.in);
 		char reponse = 'O';
+		while (reponse == 'O') {
+			System.out.println("Choisissez le mode de conversion:");
+			System.out.println("1- Convertisseur Celsius - Fahrenheit");
+			System.out.println("2- Convertisseur Fahrenheit - Celsius");
+			choix = sc.nextInt();
+			if (choix == 1) {
+				System.out.println("Température à convertir: ");
+				conversionC = sc.nextDouble();
+				resF = (9 * conversionC) / 5 + 32;
+				arrondF = arrondi(resF, 2);
+				System.out.println(conversionC + " °C correspond à " + arrondF + " °F");
+				sc.nextLine();
+			} else if (choix == 2) {
+				System.out.println("Température à convertir: ");
+				conversionF = sc.nextDouble();
+				resC = ((conversionF - 32) * 5) / 9;
+				arrondC = arrondi(resC, 2);
+				System.out.println(conversionF + " °F correspond à " + arrondC + " °C");
+				sc.nextLine();
+			} else {
+				System.out.println("Veillez choisir le bon mode de conversion...!");
+				sc.nextLine();
+			}
+			reponse = ' ';
+			while (reponse != 'O' && reponse != 'N') {
+				System.out.println("Voulez-vous continuer ? (O/N)");
+				reponse = sc.nextLine().charAt(0);
+			}
+		}
+		System.out.println("Au revoir...!");
+	}
 
+	public static double arrondi(double A, int B) {
+		return (double) ((int) (A * Math.pow(10, B) + .5)) / Math.pow(10, B);
 	}
 
 }
