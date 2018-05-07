f9d55930b2f5b34898d1ab7233194bc6cc63262b
Jean Penan <jeanpenangoumou1@gmail.com>
2018-05-08 01:18:20 +0200

    déclaration et initialisations des variables.

---
+++ TP_1_Conversion/src/Tp_Conversion.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Tp_Conversion {
+
+	public static void main(String[] args) {
+		double resC = 0.0d, resF = 0.0d, conversionC = 0.0d, conversionF = 0.0d;
+		int choix;
+		double arrondC, arrondF;
+		Scanner sc = new Scanner(System.in);
+		char reponse = 'O';
+
+	}
+
+}
